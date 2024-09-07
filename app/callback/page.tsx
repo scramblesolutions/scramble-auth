7299045829ccd52bf90917b7d16eca3f07bc4149
jaskarandeogangit <jaskaran2k15@gmail.com>
2024-09-06 21:54:50 -0700

    fix deployment bugs

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,35 +1,38 @@
 "use client";
-import { useEffect } from 'react';
-import { useSearchParams, useRouter} from 'next/navigation';
+
+import { Suspense, useEffect } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 import useOAuth from '../hooks/useOAuth';
 
 const CallbackPage = () => {
 
-  const { exchangeCodeForToken, state, error } = useOAuth();
-  const searchParams = useSearchParams();
-const router = useRouter();
+    const { exchangeCodeForToken, state, error } = useOAuth();
+    const searchParams = useSearchParams();
+    const router = useRouter();
 
-  useEffect(() => {
-    const code = searchParams.get('code');
-    if (code) {
-      exchangeCodeForToken(code);
-    }
-  }, [searchParams, exchangeCodeForToken]);
+    useEffect(() => {
+        const code = searchParams.get('code');
+        if (code) {
+            exchangeCodeForToken(code);
+        }
+    }, [searchParams, exchangeCodeForToken]);
 
 
-  useEffect(() => {
-    if (state === 'completed') {
-      router.push('/user');
-    }
-  }, [state, router]);
+    useEffect(() => {
+        if (state === 'completed') {
+            router.push('/user');
+        }
+    }, [state, router]);
 
-  return (
-    <div>
-      {state === 'exchanging' && <p>Exchanging code for token...</p>}
-      {state === 'fetching_user_info' && <p>Fetching user info...</p>}
-      {error && <p>{error}</p>}
-    </div>
-  );
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <div>
+                {state === 'exchanging' && <p>Exchanging code for token...</p>}
+                {state === 'fetching_user_info' && <p>Fetching user info...</p>}
+                {error && <p>{error}</p>}
+            </div>
+        </Suspense>
+    );
 };
 
 export default CallbackPage;
